ad98de3c79bd26cc06a15cbe9335446fd671cdb9
zyzisyz <zyziszy@foxmail.com>
2018-10-01 18:04:58 +0800

    update face detection script

---
+++ face_detection/fdn.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+
+import cv2
+
+
+def Get_Face_Num(imgPath):
+
+    # 加载图片
+    img = cv2.imread(imgPath)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 人脸识别的训练数据
+    face_cascade = cv2.CascadeClassifier(r'./model.xml')
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=3,
+        minNeighbors=5,
+        minSize=(5, 5),
+    )
+    print("发现{0}个人脸!".format(len(faces)))
+    return len(faces)
+
+
+if __name__ == "__main__":
+    while True:
+        path = input("path:")
+        if path == "q" or path == "Q":
+            break
+        else:
+            Get_Face_Num(path)
