8e05de7227b14db1f0cdc4d67ab097c79dbf39c0
zyz <zyziszy@foxmail.com>
2018-10-05 21:18:19 +0800

    add and update some code

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # DC
 
+Building...
+
+---
+
+## Packages
+
 1. face_detection——用于人脸检测
-2. speaker_recognition——用于中文语音识别，识别出中文拼音
-3. Web_crawler——爬取天气
+2. get_inoor_data——获取室内温湿度等数据
+3. speaker_recognition——用于中文语音识别，识别出中文拼音
+4. Web_crawler——爬取天气、新闻
---
+++ run.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# *************************************************************************
+#	> File Name: asr.py
+#	> Author: Yang Zhang
+#	> Mail: zyziszy@foxmail.com
+#	> Created Time: Tue 05 Oct 2018 21:16:33 PM CST
+# ************************************************************************/
+
+
+if __name__ == "__main__":
+    print("Start!")
